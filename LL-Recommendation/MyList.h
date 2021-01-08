3edf459ccfa4fa538608cff3adc7b96b98517372
Blane Staskiewicz <bas6248@psu.edu>
2021-01-08 00:01:51 -0500

    Add files via upload

---
+++ LL-Recommendation/MyList.h
@@ -0,0 +1,39 @@
+using namespace std;
+class Node {
+  public:
+    string first;
+    string second;
+    Node *next;
+
+    Node (string fst, string snd) {
+      first = fst;
+      second = snd;
+      next = NULL;
+    }
+};
+class MyLinkedList {
+  public:
+  Node *head;
+  Node *tail;
+
+  MyLinkedList() {
+    head = NULL;
+    tail = NULL;
+  }
+  void insert(Node *newNode) {
+    if (tail == NULL) {
+      head = newNode;
+    }
+    else {
+      tail->next = newNode;
+    }
+    tail = newNode;
+  }
+  void printList() {
+    Node *temp = head;
+    while (temp != tail) {
+      cout << temp->first << " " << temp->second << endl;
+      temp = temp->next;
+    }
+  }
+};
