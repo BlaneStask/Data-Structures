27425861bfca50a769e8da07dec920220ca09983
Blane Staskiewicz <bas6248@psu.edu>
2021-01-09 00:02:08 -0500

    Update MyList.h

--- LL-Recommendation/MyList.h
+++ LL-Recommendation/MyList.h
@@ -1,39 +1,41 @@
 using namespace std;
-class Node {
+
+class Node{
   public:
     string first;
     string second;
     Node *next;
 
-    Node (string fst, string snd) {
+    Node (string fst, string snd){
       first = fst;
       second = snd;
       next = NULL;
     }
 };
-class MyLinkedList {
-  public:
-  Node *head;
-  Node *tail;
 
-  MyLinkedList() {
-    head = NULL;
-    tail = NULL;
-  }
-  void insert(Node *newNode) {
-    if (tail == NULL) {
-      head = newNode;
+class MyLinkedList{
+  public:
+    Node *head;
+    Node *tail;
+
+    MyLinkedList(){
+      head = NULL;
+      tail = NULL;
     }
-    else {
-      tail->next = newNode;
+    void insert(Node *newNode){
+      if(tail == NULL){
+        head = newNode;
+      }
+      else{
+        tail->next = newNode;
+      }
+      tail = newNode;
     }
-    tail = newNode;
-  }
-  void printList() {
-    Node *temp = head;
-    while (temp != tail) {
-      cout << temp->first << " " << temp->second << endl;
-      temp = temp->next;
+    void printList(){
+      Node *temp = head;
+      while(temp != tail){
+        cout << temp->first << " " << temp->second << endl;
+        temp = temp->next;
+      }
     }
-  }
 };
