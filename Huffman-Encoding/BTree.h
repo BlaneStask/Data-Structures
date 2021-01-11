ef1021904cbdf6aa7f3e4296c6a0786c6d7f9bd5
Blane Staskiewicz <bas6248@psu.edu>
2021-01-11 00:16:19 -0500

    Update BTree.h

--- Huffman-Encoding/BTree.h
+++ Huffman-Encoding/BTree.h
@@ -1,54 +1,49 @@
-
 using namespace std;
 
-class Node {
-  public:
-  int frequency;
-  char letter;
-  Node *left;
-  Node *right;
+class Node{
+    public:
+	  int frequency;
+	  char letter;
+	  Node *left;
+	  Node *right;
 
-  Node(char l, int f) {
-    letter = l;
-    frequency = f;
-    left = right = NULL;
-  }
+	  Node(char l, int f){
+	    letter = l;
+	    frequency = f;
+	    left = right = NULL;
+	  }
 
-  void print(int indent) {
-    cout << letter << ":" << frequency << endl;
-    
-    if (right != NULL) {
-      for (int i = 0; i < indent ; i++ ) cout << "\t";
-      right->print(indent + 1);
-    }
+	  void print(int indent){
+	    cout << letter << " : " << frequency << endl;
+	    if(right != NULL){
+	      for (int i = 0; i < indent ; i++ ) cout << "\t";
+	      right->print(indent + 1);
+	    }
+	    if(left != NULL){
+	      for (int i = 0; i < indent ; i++ ) cout << "\t";
+	      left->print(indent + 1);
+	    }
+	  }
 
-    if (left != NULL) {
-      for (int i = 0; i < indent ; i++ ) cout << "\t";
-      left->print(indent + 1);
-    }
-  }
+	  void traverse(string s){
+	    // if the node is a leaf node, print out the letter and the code
+	    if(left == NULL & right == NULL)
+	      cout << letter << " : " << s << endl;
 
-  void traverse(string s) {
-	// if the node is a leaf node, print out the letter and the code
-    if (left == NULL & right == NULL)
-      cout << letter << ":" << s << endl;
-    
-	// otherwise, as an internal node
-    else if (left != NULL) {
-      left->traverse(s + '0');
-      if (right != NULL) 
-        right->traverse(s + '1');
-    }  
-  }
-
+	    // otherwise, as an internal node
+	    else if (left != NULL){
+	      left->traverse(s + '0');
+	      if(right != NULL) 
+		right->traverse(s + '1');
+	    }  
+	  }
 };
 
-class BTree {
-  public:
-  Node *root;
-  
-  BTree(){};
-  BTree(Node *r){
-    root = r;
-  }
+class BTree{
+    public:
+	  Node *root;
+	  BTree(){};
+	  BTree(Node *r){
+	    root = r;
+	  }
 };
