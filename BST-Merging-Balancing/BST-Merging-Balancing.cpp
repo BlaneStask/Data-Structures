aa2b0f382132f736df3aa8b5403d15ae46f6da08
Blane Staskiewicz <bas6248@psu.edu>
2021-01-15 00:01:00 -0500

    Update BST-Merging-Balancing.cpp

--- BST-Merging-Balancing/BST-Merging-Balancing.cpp
+++ BST-Merging-Balancing/BST-Merging-Balancing.cpp
@@ -2,18 +2,15 @@
 #include "BTree.h"
 
 using namespace std;
-int main() {
+int main(){
   int n = 9;
-  int a[] = {5, 10, 20, 15, 30, 25, 40, 23, 28} ;
-
+  int a[] = {5, 10, 20, 15, 30, 25, 40, 23, 28};
   BTree *tree = new BTree();
-
-  for (int i = 0; i < 9 ; i ++) {
+  for(int i = 0; i < 9; i++){
     tree->insert(a[i]);
   }
-
   tree->root->print(0);
   tree->createBackbone();
   tree->root->print(0);
-
+  return 0;
 }
