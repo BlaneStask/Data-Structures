9452c7e6c999a47eb8404a321de8ebf1eec5c336
Blane Staskiewicz <bas6248@psu.edu>
2021-01-17 01:31:17 -0500

    Update BST-Splay-Semi-Splay.cpp

--- BST-Splay-Semi-Splay/BST-Splay-Semi-Splay.cpp
+++ BST-Splay-Semi-Splay/BST-Splay-Semi-Splay.cpp
@@ -1,21 +1,17 @@
 #include <iostream>
 #include "BTree.h"
-
 using namespace std;
-int main() {
-  int n = 11;
-  int a[] = {'P', 'Q', 'F', 'R', 'E', 'S', 'D', 'T', 'C', 'A', 'B'} ;
 
+int main(){
+  int n = 11;
+  int a[] = {'P', 'Q', 'F', 'R', 'E', 'S', 'D', 'T', 'C', 'A', 'B'};
   BTree *tree = new BTree();
   tree->insertZigZig(a, n);
-
-  tree->print();
-
-  tree->semisplay('T');
   tree->print();
   tree->semisplay('T');
   tree->print();
-
+  tree->semisplay('T');
+  tree->print();
   n = 15;
   int b[] = {'g','f','H','e','G','d','F','c','E','b','D','a','C','A','B'};
   tree = new BTree();
@@ -23,15 +19,10 @@ int main() {
   tree->print();
   tree->splay('a');
   tree->print();
-
   int c[] = {'g','f','H','A','e','d','G','B','c','b','F','C','a','D','E'};
   tree = new BTree();
   tree->insertZigZag(c, n);
   tree->print();
   tree->splay('a');
   tree->print();
-
-  
-
-
 }
